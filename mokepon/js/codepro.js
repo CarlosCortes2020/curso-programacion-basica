ba1a25b3057c150bf286f5aa01bbb6df0779f4ca
ccortes <cicc81@gmail.com>
2022-09-16 14:43:57 -0500

    Added code to disccount life numbers of mokepon,

--- mokepon/js/codepro.js
+++ mokepon/js/codepro.js
@@ -1,6 +1,8 @@
 //*Variables globales*
 let ataqueJugador 
 let ataqueEnemigo
+let vidasJugador =3
+let vidasEnemigo = 3
 
 
 //*Funciones de de juego*
@@ -102,16 +104,27 @@ function ataqueAleatorioEnemigo(){
 }
 
 function combate(){
+    let spanVidasJugador = document.getElementById('vidas-jugador')
+    let spanVidasEnemigo = document.getElementById('vidas-enemigo')
+
     if (ataqueEnemigo == ataqueJugador){
         crearMensaje("EMPATE")
     }else if (ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA'){
         crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     }else if (ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO'){
         crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     }else if (ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA'){
         crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     }else {
         crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
     }
 
 
