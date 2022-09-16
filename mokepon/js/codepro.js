8163ef3cfbf5c4f4cc0cfcefe9209954ebd363da
ccortes <cicc81@gmail.com>
2022-09-16 09:41:12 -0500

    Agregado codigo para la seleccion de mascota e interaccion con alert y boton

--- mokepon/js/codepro.js
+++ mokepon/js/codepro.js
@@ -4,7 +4,30 @@ function iniciarJuego(){
 }
 
 function seleccionarMascotaJugador(){
-alert("SELECCIONASTE TU MASCOTA !!!")
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let inputLangostelvis = document.getElementById('langostelvis')
+    let inputTucapalma = document.getElementById('tucapalma')
+    let inputPydos = document.getElementById('pydos')
+    
+
+    if (inputHipodoge.checked){
+        alert("SELECCIONASTE A HIPODOGE !!!")
+    }else if (inputCapipepo.checked){
+        alert("SELECCIONASTE A CAPIPEPO !!!")
+    }else if (inputRatigueya.checked){
+        alert("SELECCIONASTE A RATIGUEYA !!!")
+    }else if (inputLangostelvis.checked){
+        alert("SELECCIONASTE A LANGOSTELVIS !!!")
+    }else if (inputTucapalma.checked){
+        alert("SELECCIONASTE A TUCAPALMA !!!")
+    }else if (inputPydos.checked){
+        alert("SELECCIONASTE A PYDOS !!!")
+    }else{
+        alert("NO SELECCIONASTE MASCOTA !!!")
+    }
+    
 }
 
 window.addEventListener('load',iniciarJuego)
